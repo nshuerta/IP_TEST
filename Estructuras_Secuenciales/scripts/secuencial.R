fde6dc9309389be9d1e9530ad98deedfce1c5e41
Sofia Huerta <nehuerta@uv.mx>
2018-03-18 18:37:53 -0600

    Nuevo elemento -  secuencial

---
+++ Estructuras_Secuenciales/scripts/secuencial.R
@@ -0,0 +1,6 @@
+# Se presentan las calificaciones de 5 estudiantes 
+# (75, 69, 98, 87, 67), por favor realiza lo siguiente: 
+# 1)Promedio
+# 2) Desviación estándar
+# 3) Coeficiente de Variación 
+# 4) Intervalo de confianza al 95% por la regla empírica
