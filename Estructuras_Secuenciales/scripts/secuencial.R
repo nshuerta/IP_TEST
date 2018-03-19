444a517a3c52f22856bbc155e4643a9bdcb7d5e5
Sofia Huerta <nehuerta@uv.mx>
2018-03-19 08:02:14 -0600

    Carga de instrucciones

--- Estructuras_Secuenciales/scripts/secuencial.R
+++ Estructuras_Secuenciales/scripts/secuencial.R
@@ -1,2 +1,9 @@
 # Escribe tu algoritmo y guardalo en el escritorio 
 # como Nombre_PrimerApellido_ES1
+# ----------------------------------------------------
+# Calificaciones de 5 estudiantes (75, 69, 98, 87, 67)
+# Realiza lo siguiente: 
+# 1) Promedio 
+# 2) Desviacion Estandar 
+# 3) Coeficiente de Variacion
+# 4) Intervalo de confianza al 95% R.E.
--- Estructuras_Selectivas/scripts/selectiva.R
+++ Estructuras_Selectivas/scripts/selectiva.R
@@ -1,2 +1,9 @@
 # Escribe tu algoritmo y guardalo en el escritorio 
 # como Nombre_PrimerApellido_ES2
+# -------------------------------------------------
+# Calificaciones de 3 estudiantes (69, 98, 87) 
+# Realiza lo siguiente: 
+# 1) Maximo
+# 2) Minimo
+# 3) Rango 
+# 4) Rango medio
