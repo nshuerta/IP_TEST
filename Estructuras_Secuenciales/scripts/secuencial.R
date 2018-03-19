d7a4873f3f65a248bcd80db8c96cf8f6d2ea74ac
Sofia Huerta <nehuerta@uv.mx>
2018-03-19 07:34:39 -0600

    Cambio de instrucciones

--- Estructuras_Secuenciales/scripts/secuencial.R
+++ Estructuras_Secuenciales/scripts/secuencial.R
@@ -1,6 +1,2 @@
-# Se presentan las calificaciones de 5 estudiantes 
-# (75, 69, 98, 87, 67), por favor realiza lo siguiente: 
-# 1)Promedio
-# 2) Desviación estándar
-# 3) Coeficiente de Variación 
-# 4) Intervalo de confianza al 95% por la regla empírica
+# Escribe tu algoritmo y guardalo en el escritorio 
+# como Nombre_PrimerApellido_ES1
--- Estructuras_Selectivas/scripts/selectiva.R
+++ Estructuras_Selectivas/scripts/selectiva.R
@@ -1,6 +1,2 @@
-# Se presentan las calificaciones de 3 estudiantes 
-# (69, 98, 87), por favor obtén lo siguiente: 
-# 1) Máximo 
-# 2) Mínimo 
-# 3) Rango 
-# 4) Rango medio
+# Escribe tu algoritmo y guardalo en el escritorio 
+# como Nombre_PrimerApellido_ES2
